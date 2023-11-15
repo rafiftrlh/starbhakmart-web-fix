58678f8de8d9600d3fc74e81cff95fb834187ef0
rafiftrlh <rafifitro89@gmail.com>
2023-11-15 11:40:41 +0700

    menambahkan fungsi remove di cart

--- src/app/script.js
+++ src/app/script.js
@@ -1,7 +1,6 @@
 const CardBody = document.querySelectorAll(".CardBody");
 CardBody.forEach(function (card) {
-  card.addEventListener("click", (el) => {
-    console.log(card);
+  card.addEventListener("click", () => {
     const name = card.querySelector("#name-product").textContent;
     const harga = card.querySelector("#harga").textContent;
     const containerList = document.getElementById("container-list");
@@ -13,14 +12,19 @@ function InsertList(name, harga) {
   return `<li class="liCard">
             <div class="w-full">
                 <div class="text-xs flex justify-between">
-                    <span id="listNm"><strong>${name}</strong></span>
+                    <span class="font-semibold" id="nameCart">${name}</span>
                     <span id="qty" class="text-gray-500 font-semibold">Qty: 1</span>
                 </div>
                 <div class="text-sm">
-                    <span><strong>Total: ${harga}</strong></span>
+                    <span class="font-semibold" id="hargaCart">Total: ${harga}</span>
                 </div>
             </div>
-            <i class='bx bx-trash bx-sm transition cursor-pointer bg-black/5 hover:bg-black/10 rounded-full p-1'
-                id="trash"></i>
+            <i class='bx bx-trash bx-sm transition cursor-pointer bg-black/5 hover:bg-black/10 rounded-full p-1 trashBtn'></i>
           </li>`;
 }
+
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("trashBtn")) {
+    e.target.parentElement.remove();
+  }
+});
