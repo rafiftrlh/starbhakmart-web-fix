49fd390f496c4e7efd4a0a6039ee66f15619f5a8
rafiftrlh <rafifitro89@gmail.com>
2023-11-19 13:40:40 +0700

    mengubah data menjadi menggunakan json dan membuat fungsi hitung cart

--- src/app/script.js
+++ src/app/script.js
@@ -1,69 +1,73 @@
-const CardBody = document.querySelectorAll(".CardBody");
-CardBody.forEach(function (card) {
-  card.addEventListener("click", () => {
-    const name = card.querySelector("#name-product").textContent;
-    const harga = card.querySelector("#harga").textContent;
-    const containerList = document.getElementById("container-list");
-    containerList.innerHTML += InsertList(name, harga);
+function ShowMenu() {
+  $.getJSON("/json/cart.json", function (data) {
+    let menu = data.menu;
+
+    $.each(menu, function (i, data) {
+      $("#menu").append(`<div class="CardBody" data-id="${data.id}">
+        <img src="${data.image}" alt="${data.name}" class="aspect-square rounded-sm">
+          <div class="">
+              <p id="name-product" class="font-semibold">${data.name}</p>
+              <p class="text-sky-400 font-semibold text-xl">Rp. <span id="price">${data.price}</span></p>
+          </div>
+      </div>`);
+    });
+  });
+}
+ShowMenu();
+
+$(document).ready(function () {
+  const CardBody = document.querySelectorAll(".CardBody");
+  const containerList = document.getElementById("container-list");
+  CardBody.forEach(function (card) {
+    card.addEventListener("click", () => {
+      console.log(containerList.childNodes);
+      // if () {
+
+      // }
+      console.log(card);
+      const name = card.querySelector("#name-product").textContent;
+      const price = card.querySelector("#price").textContent;
+      const dataId = card.getAttribute("data-id");
+      containerList.innerHTML += InsertList(name, price, dataId);
+      hitung();
+    });
   });
 });
 
-function InsertList(name, harga) {
-  return `<li class="liCard">
+function InsertList(name, price, dataId) {
+  return `<li class="liCard" data-id="${dataId}">
             <div class="w-full">
                 <div class="text-xs flex justify-between">
                     <span class="font-semibold" id="nameCart">${name}</span>
                     <span id="qty" class="text-gray-500 font-semibold">Qty: 1</span>
                 </div>
                 <div class="text-sm">
-                    <span class="font-semibold">Total: <span class="hargaCart">${harga}</span></span>
+                    <span class="font-semibold">Total: <span class="priceCart">${price}</span></span>
                 </div>
             </div>
             <i class='bx bx-trash bx-sm transition cursor-pointer bg-black/5 hover:bg-black/10 rounded-full p-1 trashBtn'></i>
           </li>`;
 }
 
+function hitung() {
+  let totalCart = 0;
+
+  let priceCartAll = document.querySelectorAll(".priceCart");
+  priceCartAll.forEach((p) => {
+    let price = parseInt(p.textContent);
+    totalCart += price;
+  });
+
+  let stTotal = totalCart.toString();
+
+  $("#total").html(stTotal);
+  console.log(stTotal);
+}
+
 document.addEventListener("click", function (e) {
   if (e.target.classList.contains("trashBtn")) {
+    console.log(e.target.parentElement);
     e.target.parentElement.remove();
+    hitung();
   }
-});
-
-let hargaCart = document.querySelectorAll(".hargaCart");
-
-console.log(hargaCart);
-
-// $.ajax({
-//   url: "/json/cart.json",
-//   success: (result) => {
-//     const movies = result.Search;
-//     console.log(movies);
-//     let card = "";
-//     movies.forEach((m) => {
-//       card += showCard(m);
-//     });
-//     $(".movie-container").html(card);
-
-//     // ketika tombol detail di klik
-//     $(".modal-detail-btn").on("click", function () {
-//       $.ajax({
-//         url:
-//           "http://www.omdbapi.com/?apikey=c73160ef&i=" + $(this).data("imdbid"),
-//         success: (m) => {
-//           const movieDetail = showMovieDetail(m);
-//           $(".modal-body").html(movieDetail);
-//         },
-//         error: (e) => console.log(e.responseText),
-//       });
-//     });
-//   },
-//   error: (e) => console.log(e.responseText),
-// });
-
-$.ajax({
-  url: "/json/cart.json",
-  success: (result) => {
-    console.log(result);
-  },
-  error: (e) => console.log(e),
 });
